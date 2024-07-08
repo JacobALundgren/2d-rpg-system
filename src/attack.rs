JacobALundgren/rpg-system-2d

Space bar attacks
**  Behavioral change: Subject

Issues: [#8]

src/attack.rs
@@ -1,0 +1,73 @@
+use bevy::prelude::*;
+
+pub struct AttackPlugin;
+
+#[derive(Event)]
+pub struct AttackEvent {
+    pub entity: Entity,
+}
+
+#[derive(Component)]
+pub struct Facing(pub Vec2);
+
+#[derive(Component)]
+pub struct Attack {
+    duration: Timer,
+}
+
+impl Default for Attack {
+    fn default() -> Self {
+        Self {
+            duration: Timer::from_seconds(0.5, TimerMode::Once),
+        }
+    }
+}
+
+impl Default for Facing {
+    fn default() -> Self {
+        Self(Vec2::new(1.0, 0.0))
+    }
+}
+
+impl Plugin for AttackPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_event::<AttackEvent>();
+        app.add_systems(Update, (attack, attack_duration));
+    }
+}
+
+fn attack(mut commands: Commands, mut events: EventReader<AttackEvent>, facing: Query<&Facing>) {
+    for (attacker, direction) in events
+        .read()
+        .filter_map(|x| Some(x.entity).zip(facing.get(x.entity).ok()))
+    {
+        commands
+            .spawn(SpriteBundle {
+                transform: Transform::from_translation((direction.0 * 80.).extend(0.)),
+                sprite: Sprite {
+                    color: Color::rgb(0.5, 0.5, 0.),
+                    custom_size: Some(Vec2::new(40., 40.)),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(Attack::default())
+            .set_parent(attacker);
+    }
+}
+
+fn attack_duration(
+    mut commands: Commands,
+    mut attacks: Query<(Entity, &mut Attack)>,
+    time: Res<Time>,
+) {
+    for (entity, mut attack) in attacks.iter_mut() {
+        attack.duration.tick(time.delta());
+        if attack.duration.finished() {
+            commands
+                .get_entity(entity)
+                .expect("should find entity of attack currently expiring")
+                .despawn();
+        }
+    }
+}

src/player.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
+use crate::attack::{AttackEvent, Facing};
+
 #[derive(Component, Default)]
 pub struct Player {}
 
@@ -24,23 +26,24 @@ fn setup(mut commands: Commands) {
         .insert(AdditionalMassProperties::Mass(10.))
         .insert(Player::default())
         .insert(RigidBody::Dynamic)
+        .insert(Facing::default())
         .insert(Velocity::default());
 }
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup)
-            .add_systems(Update, player_movement_system);
+            .add_systems(Update, (player_movement_system, actions));
     }
 }
 
 fn player_movement_system(
     keyboard_input: Res<Input<KeyCode>>,
-    mut player_query: Query<(&Player, &mut Velocity)>,
+    mut player_query: Query<(&mut Velocity, &mut Facing), With<Player>>,
 ) {
     const SPEED: f32 = 384.;
 
-    if let Ok((_, mut velocity)) = player_query.get_single_mut() {
+    if let Ok((mut velocity, mut facing)) = player_query.get_single_mut() {
         let mut direction = Vec2::ZERO;
 
         if keyboard_input.pressed(KeyCode::D) {
@@ -56,8 +59,25 @@ fn player_movement_system(
             direction.y -= 1.;
         }
 
-        direction = direction.normalize_or_zero();
-        direction *= SPEED;
-        velocity.linvel = direction;
+        if let Some(normalized) = direction.try_normalize() {
+            *facing = Facing(normalized);
+        }
+        velocity.linvel = direction.normalize_or_zero() * SPEED;
+    }
+}
+
+fn actions(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut event_writer: EventWriter<AttackEvent>,
+    player: Query<Entity, With<Player>>,
+) {
+    let player = match player.get_single().ok() {
+        Some(x) => x,
+        None => return,
+    };
+    for &input in keyboard_input.get_just_pressed() {
+        if input == KeyCode::Space {
+            event_writer.send(AttackEvent { entity: player });
+        };
     }
 }

JacobALundgren/2d-rpg-system

src/attack.rs
@@ -1,0 +1,73 @@
+use bevy::prelude::*;
+
+pub struct AttackPlugin;
+
+#[derive(Event)]
+pub struct AttackEvent {
+    pub entity: Entity,
+}
+
+#[derive(Component)]
+pub struct Facing(pub Vec2);
+
+#[derive(Component)]
+pub struct Attack {
+    duration: Timer,
+}
+
+impl Default for Attack {
+    fn default() -> Self {
+        Self {
+            duration: Timer::from_seconds(0.5, TimerMode::Once),
+        }
+    }
+}
+
+impl Default for Facing {
+    fn default() -> Self {
+        Self(Vec2::new(1.0, 0.0))
+    }
+}
+
+impl Plugin for AttackPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_event::<AttackEvent>();
+        app.add_systems(Update, (attack, attack_duration));
+    }
+}
+
+fn attack(mut commands: Commands, mut events: EventReader<AttackEvent>, facing: Query<&Facing>) {
+    for (attacker, direction) in events
+        .read()
+        .filter_map(|x| Some(x.entity).zip(facing.get(x.entity).ok()))
+    {
+        commands
+            .spawn(SpriteBundle {
+                transform: Transform::from_translation((direction.0 * 80.).extend(0.)),
+                sprite: Sprite {
+                    color: Color::srgb(0.5, 0.5, 0.),
+                    custom_size: Some(Vec2::new(40., 40.)),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(Attack::default())
+            .set_parent(attacker);
+    }
+}
+
+fn attack_duration(
+    mut commands: Commands,
+    mut attacks: Query<(Entity, &mut Attack)>,
+    time: Res<Time>,
+) {
+    for (entity, mut attack) in attacks.iter_mut() {
+        attack.duration.tick(time.delta());
+        if attack.duration.finished() {
+            commands
+                .get_entity(entity)
+                .expect("should find entity of attack currently expiring")
+                .despawn();
+        }
+    }
+}

src/player.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
+use crate::attack::{AttackEvent, Facing};
+
 #[derive(Component, Default)]
 pub struct Player {}
 
@@ -24,23 +26,24 @@ fn setup(mut commands: Commands) {
         .insert(AdditionalMassProperties::Mass(10.))
         .insert(Player::default())
         .insert(RigidBody::Dynamic)
+        .insert(Facing::default())
         .insert(Velocity::default());
 }
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup)
-            .add_systems(Update, player_movement_system);
+            .add_systems(Update, (player_movement_system, actions));
     }
 }
 
 fn player_movement_system(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut player_query: Query<(&Player, &mut Velocity)>,
+    mut player_query: Query<(&mut Velocity, &mut Facing), With<Player>>,
 ) {
     const SPEED: f32 = 384.;
 
-    if let Ok((_, mut velocity)) = player_query.get_single_mut() {
+    if let Ok((mut velocity, mut facing)) = player_query.get_single_mut() {
         let mut direction = Vec2::ZERO;
 
         if keyboard_input.pressed(KeyCode::KeyD) {
@@ -56,9 +59,26 @@ fn player_movement_system(
             direction.y -= 1.;
         }
 
-        direction = direction.normalize_or_zero();
-        direction *= SPEED;
-        velocity.linvel = direction;
+        if let Some(normalized) = direction.try_normalize() {
+            *facing = Facing(normalized);
+        }
+        velocity.linvel = direction.normalize_or_zero() * SPEED;
+    }
+}
+
+fn actions(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut event_writer: EventWriter<AttackEvent>,
+    player: Query<Entity, With<Player>>,
+) {
+    let player = match player.get_single().ok() {
+        Some(x) => x,
+        None => return,
+    };
+    for &input in keyboard_input.get_just_pressed() {
+        if input == KeyCode::Space {
+            event_writer.send(AttackEvent { entity: player });
+        };
     }
 }
 